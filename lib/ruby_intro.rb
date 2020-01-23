a412c470eb8a115dfb64cd47052859a6fe144217
Rahul Rana <rahulrana@MightyMac.local>
2020-01-22 23:52:56 -0600

    completed part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,16 +2,37 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+    # sum each item if array length is greater than zero, then return sum
+    sum = 0
+    if arr.length > 0
+        arr.each do |item|
+            sum += item
+        end
+    end
+    return sum
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(arr)
+    # two edge cases and then returning the sum of max two numbers
+  if arr.empty?
+      return 0
+  end
+  if arr.length == 1
+      return arr.first
+  end
+  return arr.max(2).sum
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr, n)
+    # couple edge cases, then using combination function to make pairs of each combination of elements, then check if sum = n
+    if arr.empty?
+        return false
+    end
+    if arr.length == 1
+        return false
+    end
+    return arr.combination(2).any? {|a,b| (a + b) == n}
 end
 
 # Part 2
